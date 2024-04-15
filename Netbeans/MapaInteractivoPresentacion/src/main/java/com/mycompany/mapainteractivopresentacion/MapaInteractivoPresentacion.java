36737f3500b261c00988744a365765c58bbe8fcb
melle23 <molinaestradaesmeralda@gmail.com>
2024-04-14 19:22:18 -0700

    Cambios

---
+++ Netbeans/MapaInteractivoPresentacion/src/main/java/com/mycompany/mapainteractivopresentacion/MapaInteractivoPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mapainteractivopresentacion;
+
+/**
+ *
+ * @author molin
+ */
+public class MapaInteractivoPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
